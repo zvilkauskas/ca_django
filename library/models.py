b2c99a76a1af5e86bd5c689afe20230092efcc71
zvilkauskas <zilvinas.vilkauskas@gmail.com>
2023-04-24 21:52:30 +0300

    add admin panel

--- library/models.py
+++ library/models.py
@@ -10,6 +10,10 @@ class Genre(models.Model):
     def __str__(self):
         return self.name
 
+    class Meta:
+        verbose_name = 'Genre'
+        verbose_name_plural = 'Genres'
+
 
 class Author(models.Model):
     author_id = models.AutoField(primary_key=True)
@@ -22,10 +26,13 @@ class Author(models.Model):
     def __str__(self):
         return f"{self.first_name} - {self.last_name}"
 
+    def display_books(self):
+        return ', '.join(book.title for book in self.books.all())
+
 
 class Book(models.Model):
     book_id = models.AutoField(primary_key=True)
-    author = models.ForeignKey(Author, on_delete=models.SET_NULL, null=True)
+    author = models.ForeignKey(Author, on_delete=models.SET_NULL, null=True, related_name='books')
     title = models.CharField('Title', max_length=200, help_text='Enter title of the book: ')
     description = models.TextField('Description', max_length=1000, help_text='Enter short description of the book: ')
     isbn = models.CharField(
@@ -36,6 +43,9 @@ class Book(models.Model):
 
     def __str__(self):
         return self.title
+
+    def display_genre(self):
+        return ', '.join(genre.name for genre in self.genre.all())
 
 
 class BookInstance(models.Model):
